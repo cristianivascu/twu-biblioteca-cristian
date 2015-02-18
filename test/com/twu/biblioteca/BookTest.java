f5d8d244246553967ab29be363f11f68506026e0
Cristian Ivascu <cristi.ivascu@yahoo.com>
2015-02-18 13:59:09 +0000

    extended book equality test

--- test/com/twu/biblioteca/BookTest.java
+++ test/com/twu/biblioteca/BookTest.java
@@ -11,39 +11,45 @@ import static org.junit.Assert.assertEquals;
 public class BookTest {
 
     private Book harryPotter1;
-    private Book harryPotter1Sample2;
+    private Book harryPotter1Copy;
+    private Book harryPotter2;
     Library bangaloreLibrary;
     private Map<Book, Integer> inventory = new HashMap<Book, Integer>();
 
     @Before
     public void setUp() throws Exception{
         harryPotter1 = new Book("Harry Potter and the Philosopher's Stone","J. K. Rowling", 1997);
-        harryPotter1Sample2 = new Book("Harry Potter and the Philosopher's Stone","J. K. Rowling", 1997);
+        harryPotter1Copy = new Book("Harry Potter and the Philosopher's Stone","J. K. Rowling", 1997);
+        harryPotter2 = new Book("Harry Potter and the Chamber of Secrets","J. K. Rowling", 1998);
         inventory.put(harryPotter1,1);
+        inventory.put(harryPotter2,1);
         bangaloreLibrary = new Library(inventory);
     }
 
     @Test
     public void testEquality(){
-        assertEquals(harryPotter1, harryPotter1Sample2);
+        assertEquals(harryPotter1, harryPotter1Copy);
     }
 
     @Test
     public void testEqualityWithinStandardMap(){
         HashMap<Book, Integer> testMap = new HashMap<Book, Integer>();
         testMap.put(harryPotter1,1);
-        testMap.put(harryPotter1Sample2,1);
+        testMap.put(harryPotter1Copy,1);
         assertEquals(1, testMap.size());
     }
 
     @Test
     public void testEqualityWithinLibrary(){
+        assertEquals(1, (int) bangaloreLibrary.getInventory().get(harryPotter1));
         bangaloreLibrary.addBook(harryPotter1);
-        bangaloreLibrary.addBook(harryPotter1Sample2);
+        bangaloreLibrary.addBook(harryPotter1Copy);
         assertEquals(3, (int) bangaloreLibrary.getInventory().get(harryPotter1));
-        assertEquals(3, (int) bangaloreLibrary.getInventory().get(harryPotter1Sample2));
-        assertEquals(1,  bangaloreLibrary.getInventory().size());
+        assertEquals(3, (int) bangaloreLibrary.getInventory().get(harryPotter1Copy));
+        assertEquals(1, (int) bangaloreLibrary.getInventory().get(harryPotter2));
+        assertEquals(2,  bangaloreLibrary.getInventory().size());
 
     }
+
 
 }
