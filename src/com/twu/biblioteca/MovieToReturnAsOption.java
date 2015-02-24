5c6f201928f00903bff3ebf9e5f4f929bb2f505a
Cristian Ivascu <cristi.ivascu@yahoo.com>
2015-02-24 13:50:50 +0000

    implemented return adapter for movies

--- src/com/twu/biblioteca/MovieToReturnAsOption.java
+++ src/com/twu/biblioteca/MovieToReturnAsOption.java
@@ -1,12 +1,24 @@
 package com.twu.biblioteca;
 
 public class MovieToReturnAsOption implements Option {
+    private Customer customer;
+    private Presenter presenter;
+    private Movie movie;
 
-    public MovieToReturnAsOption(Customer customer, Presenter presenter, Movie item) {
+    public MovieToReturnAsOption(Customer customer, Presenter presenter, Movie movie) {
+        this.customer = customer;
+        this.presenter = presenter;
+        this.movie = movie;
     }
 
     @Override
     public void onSelect() {
+        Message result = customer.returnMovie(movie);
+        presenter.displayMessage(result);
+    }
 
+    @Override
+    public String toString() {
+        return movie.toString();
     }
 }
---
+++ test/com/twu/biblioteca/MovieToReturnAsOptionTest.java
@@ -0,0 +1,39 @@
+package com.twu.biblioteca;
+
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.mockito.Mockito.*;
+
+public class MovieToReturnAsOptionTest {
+
+    private Customer alice;
+    private Presenter presenter;
+    private Movie godfather;
+    private MovieToReturnAsOption movieToReturnAsOption;
+
+    @Before
+    public void setUp() throws Exception{
+        godfather = new Movie("The Godfather",1972,"Francis Ford Coppola");
+        alice = mock(Customer.class);
+        presenter = mock(Presenter.class);
+        when(alice.returnMovie(godfather)).thenReturn(Message.SUCCESSFUL_MOVIE_RETURN);
+        movieToReturnAsOption = new MovieToReturnAsOption(alice, presenter, godfather);
+    }
+
+    @Test
+    public void shouldDelegateReturnWithCorrectMovie(){
+        movieToReturnAsOption.onSelect();
+        verify(alice).returnMovie(godfather);
+    }
+
+    @Test
+    public void shouldDisplayReturnResult(){
+        movieToReturnAsOption.onSelect();
+        verify(presenter).displayMessage(Message.SUCCESSFUL_MOVIE_RETURN);
+    }
+
+
+
+}
